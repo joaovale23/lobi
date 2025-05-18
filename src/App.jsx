7ee17c84d8c7694a1e3af009c1b38f44a59003ec
JoaoVale <joaovitorvalec@gmail.com>
2025-05-18 01:54:04 -0300

    início funções

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ export default function App() {
     
       {/* Luz que segue o mouse */}
       <div
-        className={`absolute inset-0 z-30 pointer-events-none transition-opacity duration-700 ${
+        className={`absolute inset-0 z-50 pointer-events-none transition-opacity duration-700 ${
           spotlightVisible ? 'opacity-100' : 'opacity-0'
         }`}
         style={spotlightStyle}
@@ -70,15 +70,15 @@ export default function App() {
 
       {spotlightTransparent && (
         <div
-          className={`pointer-events-none z-30 fixed transition-opacity duration-700 ${ joaVisible ? 'opacity-0' : 'opacity-100'}`}
+          className={`pointer-events-none z-50 fixed transition-opacity duration-700 ${ joaVisible ? 'opacity-0' : 'opacity-100'}`}
           style={{
             left: mousePosition.x - 15,
             top: mousePosition.y - 15,
-            width: 30,
-            height: 30,
+            width: 40,
+            height: 40,
             borderRadius: '50%',
             background: 'url(/images/joa.png)',
-            backgroundSize: '30px 30px',
+            backgroundSize: '35px 35px',
             backgroundRepeat: 'no-repeat',
             backgroundPosition: 'center',
             position: 'fixed',
@@ -88,7 +88,7 @@ export default function App() {
       )}
 
       {/* Conteúdo visível */}
-      <div className="min-h-screen w-full relative z-20 perspective-750 preserve-3d text-white p-8 space-y-8">
+      <div className="min-h-screen w-full perspective-750 preserve-3d text-white p-8 space-y-8">
 
         <div
           onClick={handleVelaClick}
@@ -101,7 +101,8 @@ export default function App() {
         <div
           onClick={() => handleReveal('s1')}
           onMouseLeave={() => setRevealed((prev) => ({ ...prev, s1: false }))}
-          className="absolute top-[50vh] left-[80vw] w-52 h-52 bg-[url('/images/fita.png')] bg-contain bg-no-repeat text-black p-4"
+          className="absolute top-[50vh] left-[80vw] w-44 h-52 bg-[url('/images/fita.png')] bg-contain bg-no-repeat text-black p-4
+            transition-all duration-300 hover:scale-105"
         >
           {/*revealed.s1 && (
             <div className="bg-white text-black p-4 rounded-xl shadow-lg">
@@ -114,36 +115,11 @@ export default function App() {
         <div
           onClick={() => handleReveal('s2')}
           onMouseLeave={() => setRevealed((prev) => ({ ...prev, s2: false }))}
-          className="absolute top-[70vh] left-[75vw] w-44 h-44 bg-[url('/images/copa.png')] bg-contain bg-no-repeat text-black p-4"
+          className="absolute top-[70vh] left-[72vw] w-44 h-44 rotate-x-30 bg-[url('/images/copa.png')] bg-contain bg-no-repeat text-black p-4
+            transition-all duration-300 hover:scale-105"
         >
           {/*revealed.s2 && (
-            <div className="bg-white text-black p-4 rounded-xl shadow-lg">
-              <div
-                onClick={() => quackAudioRef.current && quackAudioRef.current.play()}
-                className="absolute top-[10%] left-[90%] w-24 h-24 bg-[url('/images/quack.png')] bg-contain bg-no-repeat text-black p-4"
-              ></div>
-              <audio ref={quackAudioRef} src="/sounds/quack.mp3" />
-
-              <div
-                className="absolute top-[20%] left-[75%] w-24 h-24 bg-[url('/images/fifiu.png')] bg-contain bg-no-repeat text-black p-4"
-              ></div>
-
-              <div
-                className="absolute top-[50%] left-[65%] w-24 h-24 bg-[url('/images/miau.png')] bg-contain bg-no-repeat text-black p-4"
-              ></div>
-
-              <div
-                className="absolute top-[60%] left-[55%] w-24 h-24 bg-[url('/images/pexe.png')] bg-contain bg-no-repeat text-black p-4"
-              ></div>
-
-              <div
-                className="absolute top-[70%] left-[25%] w-24 h-24 bg-[url('/images/auau.png')] bg-contain bg-no-repeat text-black p-4"
-              ></div>
-
-              <div
-                className="absolute top-[37%] left-[60%] w-24 h-24 bg-[url('/images/cacos.png')] bg-contain bg-no-repeat text-black p-4"
-              ></div>
-            </div>
+            
           )*/}
         </div>
 
@@ -151,7 +127,8 @@ export default function App() {
         <div
           onClick={() => handleReveal('s3')}
           onMouseLeave={() => setRevealed((prev) => ({ ...prev, s3: false }))}
-          className="absolute top-[43vh] left-[0vw] w-72 h-72 bg-[url('/images/planilha.png')] bg-contain bg-no-repeat text-black p-4"
+          className="absolute top-[43vh] left-[0vw] w-52 h-72 bg-[url('/images/planilha.png')] bg-contain bg-no-repeat text-black p-4
+            transition-all duration-300 hover:scale-105"
         >
           {/*revealed.s3 && (
             <div className="bg-white text-black p-4 rounded-xl shadow-lg">
@@ -166,16 +143,37 @@ export default function App() {
         {/* Surpresa 4 */}
         <div
           onClick={() => handleReveal('s4')}
-          onMouseLeave={() => setRevealed((prev) => ({ ...prev, s4: false }))}
-          className="absolute top-[65vh] left-[10vw] w-52 h-52 rotate-x-30 bg-[url('/images/bichos.png')] bg-contain bg-no-repeat text-black p-4"
-        >
-          {/*revealed.s4 && (
-            <div className="bg-white text-black p-4 rounded-xl shadow-lg">
-              🎁 Primeira surpresa: Te amo mais do que chocolate 🍫
-            </div>
-          )*/}
-        </div>
+          className="absolute top-[65vh] left-[10vw] w-52 h-52 rotate-x-30 bg-[url('/images/bichos.png')] bg-contain bg-no-repeat text-black p-4
+            transition-all duration-300 hover:scale-105"
+        ></div>
       </div>
+
+      {/* Modal centralizado com fundo embaçado */}
+      {revealed.s4 && (
+        <div
+          className="fixed inset-0 z-30 flex items-center justify-center bg-black/60 backdrop-blur-sm transition-all duration-500"
+          onClick={() => setRevealed(prev => ({ ...prev, s4: false }))}
+        >
+          <div
+            className="relative z-40 bg-[url('/images/bickos.png')] bg-center bg-contain bg-no-repeat text-black rounded-2xl shadow-2xl p-8 flex flex-wrap items-center justify-center gap-4
+              transition-all duration-500 scale-100 opacity-100 animate-fade-in"
+            style={{ minWidth: 450, minHeight: 600, maxWidth: 450, maxHeight: 1000 }}
+            onClick={e => e.stopPropagation()} // impede fechar ao clicar dentro do modal
+          >
+            <div
+              onClick={() => quackAudioRef.current && quackAudioRef.current.play()}
+              className="w-32 h-32 bg-[url('/images/quack.png')] bg-contain bg-no-repeat"
+            ></div>
+            <audio ref={quackAudioRef} src="/sounds/quack.mp3" />
+
+            <div className="w-32 h-32 bg-[url('/images/fifiu.png')] bg-contain bg-no-repeat" />
+            <div className="w-32 h-32 bg-[url('/images/miau.png')] bg-contain bg-no-repeat" />
+            <div className="w-32 h-32 bg-[url('/images/pexe.png')] bg-contain bg-no-repeat" />
+            <div className="w-32 h-32 bg-[url('/images/auau.png')] bg-contain bg-no-repeat" />
+            <div className="w-32 h-32 bg-[url('/images/cacos.png')] bg-contain bg-no-repeat" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
